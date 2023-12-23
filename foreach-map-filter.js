e735a9442fe3e16c9ca9154a600d6d3aceebd0bf
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2023-12-23 16:01:11 -0500

    rmv'd cmmts and started next function

--- foreach-map-filter.js
+++ foreach-map-filter.js
@@ -1,11 +1,3 @@
-/*
-Write a function called doubleValues which accepts an array and returns a new array with all the values in the array passed to the function doubled
-
-Examples:
-    doubleValues([1,2,3]) // [2,4,6]
-    doubleValues([5,1,2,3,10]) // [10,2,4,6,20]
-
-*/
 function doubleValues(arr) {
   const newArr = [];
   for (let i = 0; i < arr.length; i++) {
@@ -14,14 +6,6 @@ function doubleValues(arr) {
   return newArr;
 }
 
-/*
-Write a function called onlyEvenValues which accepts an array and returns a new array with only the even values in the array passed to the function
-
-Examples:
-    onlyEvenValues([1,2,3]) // [2]
-    onlyEvenValues([5,1,2,3,10]) // [2,10]
-
-*/
 function onlyEvenValues(arr) {
   const newArr = [];
   for (let i = 0; i < arr.length; i++) {
@@ -39,7 +23,13 @@ Examples:
     showFirstAndLast(['hi', 'goodbye', 'smile']) // ['hi', 'ge', 'se']
 
 */
-function showFirstAndLast(arr) {}
+function showFirstAndLast(arr) {
+  newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(arr[i[0]]);
+  }
+  return newArr;
+}
 
 /*
 Write a function called addKeyAndValue which accepts an array of objects, a key, and a value and returns the array passed to the function with the new key and value added for each object 
